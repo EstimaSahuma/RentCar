b9fb8b981b8dfae439bddf2bce1d8b665d6af9bd
EstimaSahuma <estimaetilenoglicol@gmail.com>
2022-07-19 09:14:45 -0300

    ...

--- api/views.py
+++ api/views.py
@@ -4,13 +4,16 @@ from django.db.models import F, Q, Sum
 from .models import Revendedor
 from .serializers import RevendedorSerializer
 
-from rest_framework import serializers
+from rest_framework import viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.viewsets import ReadOnlyModelViewSet
+from rest_framework import generics
+from rest_framework.generics import get_object_or_404
 
 # Create your views here.
-class RevendedorViewSet(ReadOnlyModelViewSet):
-    model = Revendedor
+class RevendedorViewSet(generics.ModelViewSet):
     serializer_class = RevendedorSerializer
     queryset = Revendedor.objects.all()
+
+    
